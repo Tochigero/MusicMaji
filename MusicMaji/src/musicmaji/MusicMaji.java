a296445d17ddda977bd3f859c99252c9ec7d469f
Wiki59 <wiki59@live.fr>
2015-09-02 16:06:08 +0200

    no edit

---
+++ MusicMaji/src/musicmaji/MusicMaji.java
@@ -0,0 +1,18 @@
+package musicmaji;
+
+/**
+ *
+ * @author Wissam
+ */
+
+
+public class MusicMaji {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
