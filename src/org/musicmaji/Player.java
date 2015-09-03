58c2f986c49348284390873d6345f35638de700a
Alexandre <Alexandre@10.19.1.163>
2015-09-03 09:29:07 +0200

    Création Class "Player" : pour lire .mp3

---
+++ src/org/musicmaji/Player.java
@@ -0,0 +1,10 @@
+package org.musicmaji;
+
+import java.io.File;
+import javafx.scene.media.MediaPlayer;
+
+public class Player {
+	String uriString = new File(fileName).toURI().toString();
+	
+	Media
+}
