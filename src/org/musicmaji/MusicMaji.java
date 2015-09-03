9dcb9c1464f3881c80ee4bf0ba2f5c54c3edd5d6
Samy DAHMANE <dahmanes@acajou26.iut-infobio.priv.univ-lille1.fr>
2015-09-03 08:53:13 +0200

    init du projet

---
+++ src/org/musicmaji/MusicMaji.java
@@ -0,0 +1,5 @@
+package org.musicmaji;
+
+public class MusicMaji {
+
+}
---
+++ src/org/musicmaji/MusicMajiTest.java
@@ -0,0 +1,12 @@
+package org.musicmaji;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MusicMajiTest {
+	@Test
+	public void should_return_bla_bla_when_bal () {
+		Assert.assertTrue(true);
+	}
+
+}
