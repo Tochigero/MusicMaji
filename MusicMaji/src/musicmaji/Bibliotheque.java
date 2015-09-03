9656202781c9f38b0734d08e683c25e65e370ce4
Wiki59 <wiki59@live.fr>
2015-09-03 08:32:19 +0200

    main page

---
+++ MusicMaji/src/musicmaji/Bibliotheque.java
@@ -0,0 +1,12 @@
+package musicmaji;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Bibliotheque extends JFrame {
+
+    {
+        setSize(new Dimension(600, 600));
+        setVisible(true);
+    }
+}
--- MusicMaji/src/musicmaji/MusicMaji.java
+++ MusicMaji/src/musicmaji/MusicMaji.java
@@ -10,9 +10,7 @@ public class MusicMaji {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world !!!");
-        System.out.println("Ceci est la main page");
-        System.out.println("Bob");
+            
     }
 
 }
